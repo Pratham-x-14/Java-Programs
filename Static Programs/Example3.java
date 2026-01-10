ccc18cb6f8bbb76769803973c30457a5101067af
Pratham-x-14 <shah.x014@gmail.com>
2026-01-10 20:10:51 +0530

    New Programs Added.

---
+++ Static Programs/Example3.java
@@ -0,0 +1,21 @@
+public class Example3 
+{
+    static int a = m1();  
+
+    static int m1() 
+    {
+        System.out.println("m1 called");
+        return 30;
+    }
+
+    static 
+    {
+        System.out.println("Static block executed");
+        a += 20;
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Final value of a: " + a);
+    }
+}
