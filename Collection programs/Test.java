a24b61ef1cbd0074ee129ecd8769675c995c9c49
Pratham-x-14 <shah.x014@gmail.com>
2026-02-06 14:11:42 +0530

    collection program added.

---
+++ Collection programs/Test.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+import java.util.Vector;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Collection collection = new Vector<>();
+
+        collection.add("pratham@mail.com");
+        collection.add("prathamShah@mail.com");
+
+        System.out.println(collection);
+    }
+}
