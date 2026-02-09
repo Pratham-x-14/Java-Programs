90e51098b70d15797e73fe2195fe8c5b10d108ba
Pratham-x-14 <shah.x014@gmail.com>
2026-02-09 22:25:20 +0530

    Palindrome program added.

---
+++ Palindrome.java
@@ -0,0 +1,33 @@
+public class Palindrome 
+{
+    public static void main(String args[])
+    {
+        String str = "madam";
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = str.length()-1;
+
+        while(start<end)
+        {
+            if(str.charAt(start) != str.charAt(end))
+            {
+                isPalindrome = false;
+                break;
+            }
+            else
+            {
+                start++;
+                end--;
+            }
+        }
+        if(isPalindrome)
+        {
+            System.out.println(str+" is a palindrome.");
+        }
+        else
+        {
+            System.out.println(str+" is not a palindrome.");
+        }
+    }
+}
