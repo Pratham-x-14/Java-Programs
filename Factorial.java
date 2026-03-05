2b72874d3704ceaa00bb8bdf336b62b4431590a0
Pratham-x-14 <shah.x014@gmail.com>
2026-03-05 17:03:39 +0530

    New program added.

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());   // Reading input from STDIN
+
+        int fact = 1;
+
+        for(int i = 1 ; i<=n ; i++)
+        {
+            fact = fact*i;
+        }
+        System.out.println(fact);    // Writing output to STDOUT
+    }
+}
